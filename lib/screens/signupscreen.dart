47d5353b5833e72f067550caff04bc1a98a141cf
Doğaç Evcin <dogacevcin2008@gmail.com>
2025-04-07 22:01:37 +0300

    Sign Up System

--- lib/screens/signupscreen.dart
+++ lib/screens/signupscreen.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:marketapp/data/constants.dart';
+import 'package:marketapp/main.dart';
+import 'package:marketapp/screens/mainscreen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -13,6 +17,8 @@ class _SignupScreenState extends State<SignupScreen> {
   TextEditingController emailController = TextEditingController();
   bool allowed = false;
   TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController surnameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +61,7 @@ class _SignupScreenState extends State<SignupScreen> {
                                 child: Padding(
                                   padding: const EdgeInsets.all(16.0),
                                   child: TextFormField(
-                                    controller: emailController,
+                                    controller: nameController,
                                     decoration: InputDecoration(
                                       border: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(20),
@@ -69,7 +75,7 @@ class _SignupScreenState extends State<SignupScreen> {
                                 child: Padding(
                                   padding: const EdgeInsets.all(16.0),
                                   child: TextFormField(
-                                    controller: emailController,
+                                    controller: surnameController,
                                     decoration: InputDecoration(
                                       border: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(20),
@@ -126,7 +132,47 @@ class _SignupScreenState extends State<SignupScreen> {
                         ),
                         Divider(height: 20.h, color: Colors.transparent),
                         ElevatedButton(
-                          onPressed: () {},
+                          onPressed: () async {
+                            if (allowed == true) {
+                              try {
+                                if (validatePassword(
+                                      context,
+                                      passwordController.text,
+                                    ) ==
+                                    true) {
+                                  await auth.createUserWithEmailAndPassword(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                                  Constants.pushAndRemoveUntil(
+                                    context: context,
+                                    destination: MainScreen(),
+                                  );
+                                }
+                              } on FirebaseAuthException catch (error) {
+                                if (error.message ==
+                                    "The email address is already in use by another account.") {
+                                  Constants.showSnackBar(
+                                    context: context,
+                                    message:
+                                        "This email adress is already taken",
+                                  );
+                                } else if (error.message ==
+                                    "The email address is badly formatted.") {
+                                  Constants.showSnackBar(
+                                    context: context,
+                                    message: "Please write a valid email",
+                                  );
+                                }
+                                print(error.message);
+                              }
+                            } else {
+                              Constants.showSnackBar(
+                                context: context,
+                                message: "Please check the box firstly.",
+                              );
+                            }
+                          },
                           child: Text("Sign Up"),
                         ),
                       ],
@@ -139,5 +185,33 @@ class _SignupScreenState extends State<SignupScreen> {
         ),
       ),
     );
+  }
+}
+
+bool validatePassword(BuildContext context, String password) {
+  List<String> errors = [];
+
+  if (password.length < 6) {
+    errors.add("at least 6 characters");
+  }
+  if (!RegExp(r'[a-z]').hasMatch(password)) {
+    errors.add("at least one lowercase letter");
+  }
+  if (!RegExp(r'[A-Z]').hasMatch(password)) {
+    errors.add("at least one uppercase letter");
+  }
+  if (!RegExp(r'\d').hasMatch(password)) {
+    errors.add("at least one number");
+  }
+  if (!RegExp(r'[\W_]').hasMatch(password)) {
+    errors.add("at least one special character");
+  }
+
+  if (errors.isNotEmpty) {
+    final message = "Password must contain:\n• " + errors.join("\n• ");
+    Constants.showSnackBar(context: context, message: message);
+    return false;
+  } else {
+    return true;
   }
 }
